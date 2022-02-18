4bd9adfa80fb20f6066c477eebb358d45abb8f80
Alex-Rafter <you@example.com>
2022-02-18 12:32:57 +0000

    ignore auto generated files

--- test.js
+++
@@ -1,9 +0,0 @@
-const shell = require('shelljs');
-// const urls = shell.cat('temp_edit.txt')
-// console.log(urls.split("\n"));
-
-const args = process.argv.slice(2);
-const argFile = args[0];
-const argUrl = args[1];
-
-console.log(argUrl);
--- tst_2.js
+++
@@ -1,8 +0,0 @@
-const shell = require("shelljs");
-const cheerio = require("cheerio");
-
-const args = process.argv.slice(2);
-const argFile = args[0];
-const argUrl = args[1];
-
-console.log(`argFile is ${argFile}`);
